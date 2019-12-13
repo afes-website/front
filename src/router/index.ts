44462b36f02a615b2371dddf18d57d5923f83d63
ibuki2003 <ibuki426.sp@gmail.com>
2019-12-13 19:31:42 +0900

    improve routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +7,7 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue")
   },
   {
     path: "/about",
@@ -16,8 +15,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "@/views/About.vue")
   }
 ];
 
