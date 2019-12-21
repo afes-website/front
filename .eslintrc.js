a105baa8c8a0c96b5f74cf187dbe568130f7ef43
ibuki2003 <ibuki426.sp@gmail.com>
2019-12-21 17:24:03 +0900

    add prettier plugin into eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     node: true
   },
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  plugins: ["prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
