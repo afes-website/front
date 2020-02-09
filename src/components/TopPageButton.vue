e587c7b5ec50b5641082e6147cc215d2d880cf41
su8ru <su8ru.i@gmail.com>
2020-02-09 16:27:32 +0900

    [mobile] introduct material design

--- src/components/TopPageButton.vue
+++ src/components/TopPageButton.vue
@@ -13,7 +13,8 @@
   padding: 0.8rem;
   margin: 0.25rem;
   border-radius: 0.5rem;
-  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3);
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
+    0 4px 2px -2px rgba(0, 0, 0, 0.2);
   position: relative;
 
   .button-icon {
