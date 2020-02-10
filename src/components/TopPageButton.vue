a134d305d2b6fa78d22bade1372c5db8aaa597d5
su8ru <su8ru.i@gmail.com>
2020-02-10 21:35:31 +0900

    simpled TopPageButton box-shadow

--- src/components/TopPageButton.vue
+++ src/components/TopPageButton.vue
@@ -13,8 +13,7 @@
   padding: 0.8rem;
   margin: 0.25rem;
   border-radius: 0.5rem;
-  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
-    0 4px 2px -2px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 3px 4px -1px rgba(0, 0, 0, 0.4);
   position: relative;
 
   .button-icon {
