e99c6aa84f9f9da354974439fe606c9dad13e6c8
ibuki2003 <ibuki426.sp@gmail.com>
2020-02-20 20:21:32 +0900

    build: update all the packages
    BREAKING CHANGE: breaking

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,27 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
-  plugins: ["prettier"],
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
-  parserOptions: {
-    parser: "@typescript-eslint/parser"
-  },
   overrides: [
     {
-      files: ["**/__tests__/*.{j,t}s?(x)"],
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
       env: {
         mocha: true
       }
