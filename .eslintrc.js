3d462aa8ba9052d52f945258bf6dcb2f2576c644
ibuki2003 <ibuki426.sp@gmail.com>
2020-02-22 10:47:29 +0900

    chore: change no-console rule level to warn in development env

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,8 @@ module.exports = {
   },
 
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn"
   },
   overrides: [
     {
