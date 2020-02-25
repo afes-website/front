afa02c56bebd475f886a80e9f67e33a6dc7d0ef8
ibuki2003 <ibuki426.sp@gmail.com>
2020-02-25 20:43:00 +0900

    feat: add admin login modal

---
+++ src/components/AdminLoginModal.vue
@@ -0,0 +1,87 @@
+<template>
+  <b-modal
+    id="modal-1"
+    title="BootstrapVue"
+    v-model="value"
+    @ok="modal_ok"
+    @hidden="modal_cancel"
+  >
+    <b-form @submit.stop.prevent="login">
+      <b-form-group id="input-group-id" label="User ID:" label-for="input-id">
+        <b-form-input id="input-id" v-model="id" required></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-password"
+        label="Password:"
+        label-for="input-password"
+      >
+        <b-form-input
+          id="input-password"
+          v-model="password"
+          required
+          type="password"
+        ></b-form-input>
+      </b-form-group>
+    </b-form>
+
+    <template v-slot:modal-footer="{ ok }">
+      <!-- TODO: show status, improve UI/UX -->
+      <!-- <p v-if="status == 'idlle'">なにもひょうじしない</p>
+      <p v-if="status == 'pending'">くるくる</p>
+      <p v-if="status == 'success'">ちぇっく</p>
+      <p v-if="status == 'success'">ちぇっく</p>
+      <p v-if="status == 'fail'">びっくり</p> -->
+      <p>{{ status }}</p>
+      <b-button variant="primary" @click="ok">
+        Login
+      </b-button>
+    </template>
+  </b-modal>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import aspida from "@aspida/axios";
+
+import Auth from "@/libs/auth";
+import { AxiosError } from "axios";
+import EventHub from "@/libs/auth/admin_auth_eventhub";
+
+@Component
+export default class AdminLoginModal extends Vue {
+  id = "";
+  password = "";
+  status: "idle" | "pending" | "success" | "fail" = "idle";
+
+  value = false;
+
+  created() {
+    EventHub.onOpenModal(() => {
+      this.value = true;
+    });
+  }
+
+  modal_ok(e: Event) {
+    e.preventDefault();
+    this.login();
+  }
+
+  modal_cancel() {
+    EventHub.emitLoginFail();
+  }
+
+  login() {
+    this.status = "pending";
+    Auth.AdminAuth.login(aspida(), this.id, this.password)
+      .then(() => {
+        this.status = "success";
+        this.value = false;
+        EventHub.emitLoginSuccess();
+      })
+      .catch((e: AxiosError) => {
+        this.status = "fail";
+      });
+  }
+}
+</script>
