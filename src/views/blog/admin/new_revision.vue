6233c2619ee664dfade46f915047a923ead3288d
ibuki2003 <ibuki426.sp@gmail.com>
2020-02-29 00:56:19 +0900

    feat: improve new_revision page

--- src/views/blog/admin/new_revision.vue
+++ src/views/blog/admin/new_revision.vue
@@ -2,8 +2,22 @@
   <div class="box">
     <h1>{{ title }}</h1>
     <p>path:<b-input v-model="article_path" /></p>
+    <p>
+      <b-button @click="load">
+        記事情報を読みこむ
+        <fetch-status-icon :status="fetch_status" />
+      </b-button>
+    </p>
     <p>title:<b-input v-model="article_title" /></p>
-    <b-textarea v-model="content"></b-textarea>
+    <b-tabs>
+      <b-tab title="編集" active>
+        <b-textarea v-model="content" rows="30"></b-textarea>
+      </b-tab>
+      <b-tab title="プレビュー">
+        <div v-html="rendered_content"></div>
+      </b-tab>
+      <b-tab title="現在との差分" disabled></b-tab>
+    </b-tabs>
     <b-button @click="post" variant="primary">
       post
       <fetch-status-icon :status="status" small />
@@ -21,6 +35,7 @@ import WriterAuth from "@/libs/auth/writer_auth";
 import { BlogRevision } from "@/apis/blog/revisions/@types";
 import FetchStatus from "@/libs/fetch_status";
 import FetchStatusIcon from "@/components/FetchStatusIcon.vue";
+import Markdown from "@/libs/markdown";
 
 @Component({ components: { FetchStatusIcon } })
 export default class NewRevision extends Vue {
@@ -29,8 +44,26 @@ export default class NewRevision extends Vue {
   article_title = "";
   article_path = "";
   content = "";
+  latest_content = "";
 
   status: FetchStatus = "idle";
+  fetch_status: FetchStatus = "idle";
+
+  load() {
+    this.fetch_status = "pending";
+    api(aspida())
+      .blog.articles._id(this.article_path)
+      .$get()
+      .then(article => {
+        this.article_title = article.title;
+        this.latest_content = article.content;
+        this.content = article.content;
+        this.fetch_status = "idle";
+      })
+      .catch(() => {
+        this.fetch_status = "fail";
+      });
+  }
 
   post() {
     this.status = "pending";
@@ -59,6 +92,10 @@ export default class NewRevision extends Vue {
           this.status = "fail";
         });
     });
+  }
+
+  get rendered_content() {
+    return Markdown.render(this.content);
   }
 }
 </script>
