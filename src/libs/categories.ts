62cc718cb419c11ca44e56ebb55c97f0b0c6afe2
ibuki2003 <ibuki426.sp@gmail.com>
2020-03-04 22:38:25 +0900

    feat: add internal category

--- src/libs/categories.ts
+++ src/libs/categories.ts
@@ -8,7 +8,8 @@ export const categories: { [key: string]: Category } = {
   general: { name: "文実全体", visible: true },
   workTeam: { name: "分科局", visible: true },
   exh: { name: "展示団体", visible: true },
-  contrib: { name: "個人･寄稿", visible: true }
+  contrib: { name: "個人･寄稿", visible: true },
+  internal: { name: "内部生向け", visible: false }
 };
 
 export function getCategory(key: string): string {
