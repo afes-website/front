99261214edebc77ebae8e5feff66b441f010b50c
ibuki2003 <ibuki426.sp@gmail.com>
2020-03-05 21:32:52 +0900

    feat: disable service-worker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
+//import "./registerServiceWorker";
 import router from "./router";
 
 import BootstrapVue from "bootstrap-vue";
