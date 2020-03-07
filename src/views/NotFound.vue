6b7f83aed09d9261bb847da82c076e3631b3d15b
ibuki2003 <ibuki426.sp@gmail.com>
2020-03-07 12:53:03 +0900

    feat: add temprary 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <h1>{{ page_title }}</h1>
+    <section>
+      <p>404 Not Found<!-- TODO: consider message --></p>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class NotFound extends Vue {
+  readonly page_title = "404 Not Found"; // TODO: consider title
+}
+</script>
