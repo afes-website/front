f9ff11d6ebab6641b3493b74e3e4c2f94ebda9db
su8ru <s@su8ru.dev>
2020-03-09 11:52:12 +0900

    feat(contact): setup contact page component

---
+++ src/views/contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="contact" class="box">
+    <h1>{{ page_title }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Contact extends Vue {
+  readonly page_title = "お問い合わせ";
+}
+</script>
