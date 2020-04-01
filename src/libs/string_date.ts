43b1067784d06c05c7e927096b2816e0b6dc7535
su8ru <s@su8ru.dev>
2020-04-02 05:51:32 +0900

    fix: date format

--- src/libs/string_date.ts
+++ src/libs/string_date.ts
@@ -1,9 +1,9 @@
 import moment from "moment";
 
-export function getStringTime(date: Date) {
-  return moment(date).format("YYYY/MM/DD HH:mm");
+export function getStringTime(date: string) {
+  return moment(date, "YYYY-MM-DD HH:mm").format("YYYY/MM/DD HH:mm");
 }
 
-export function getStringDate(date: Date) {
-  return moment(date).format("YYYY/MM/DD");
+export function getStringDate(date: string) {
+  return moment(date, "YYYY-MM-DD HH:mm").format("YYYY/MM/DD");
 }
