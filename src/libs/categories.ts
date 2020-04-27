9100581a145715fee98bc7fb1a04ca76af327554
su8ru <s@su8ru.dev>
2020-04-28 01:08:01 +0900

    feat: cache categories

--- src/libs/categories.ts
+++ src/libs/categories.ts
@@ -1,8 +1,16 @@
 import api from "@/apis/$api";
 import aspida from "@aspida/axios";
+import { Categories } from "@/apis/blog/categories/@types";
 
-export async function getCategories() {
-  return api(aspida()).blog.categories.$get();
+let categories: Categories | undefined = undefined;
+
+export default async function getCategories(): Promise<Categories> {
+  if (categories !== undefined) return Promise.resolve(categories);
+
+  return api(aspida())
+    .blog.categories.$get()
+    .then((data) => {
+      categories = data;
+      return data;
+    });
 }
-
-export default getCategories;
