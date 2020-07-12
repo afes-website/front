1da3b48fb1a36f0b34d34ee8c8f112f0cd3389d3
subaru <s@su8ru.dev>
2020-07-12 23:28:00 +0900

    feat: show revisions in row details

--- src/views/blog/admin/path_list.vue
+++ src/views/blog/admin/path_list.vue
@@ -12,12 +12,24 @@
       hover
       :responsive="true"
       :items="paths"
-      :fields="fields"
-      :filter="filter"
+      :fields="pathFields"
+      :filter="pathFilter"
       :sort-by.sync="sortBy"
       class="mb-0"
     >
       <template v-slot:cell(waiting_count)="row">
+        <font-awesome-icon
+          icon="angle-down"
+          class="fa-fw"
+          v-if="get_is_details_showing(row)"
+          @click="toggle_details(row)"
+        />
+        <font-awesome-icon
+          icon="angle-right"
+          class="fa-fw"
+          v-else
+          @click="toggle_details(row)"
+        />
         <b-badge
           variant="warning"
           v-if="has_waiting(row)"
@@ -27,6 +39,32 @@
         >
           {{ get_waiting_count(row) }}
         </b-badge>
+      </template>
+      <template v-slot:row-details="row">
+        <b-table
+          show-empty
+          small
+          hover
+          :responsive="true"
+          :items="get_revisions(row)"
+          :fields="revisionFields"
+          sort-by="id"
+          class="mb-1"
+        >
+          <template v-slot:cell(status)="r_row">
+            <font-awesome-icon
+              :icon="get_status_icon(r_row)"
+              :id="format_revision_icon_id(r_row)"
+              class="fa-fw"
+            />
+            <b-tooltip
+              :target="format_revision_icon_id(r_row)"
+              triggers="hover"
+            >
+              {{ get_revision_status(r_row) }}
+            </b-tooltip>
+          </template>
+        </b-table>
       </template>
     </b-table>
   </div>
@@ -97,43 +135,64 @@ import { Categories } from "@/apis/blog/categories/@types";
 import getCategories from "@/libs/categories";
 import Breadcrumb from "@/components/Breadcrumb.vue";
 import { getStringTime } from "@/libs/string_date";
+import { WriterUserInfo } from "@/apis/writer/user";
+import { BvTableVariant } from "bootstrap-vue";
 
 interface Path {
   id: string;
   category?: string;
   title?: string;
-  author?: string;
-  handle_name?: string;
+  author?: WriterUserInfo;
+  handle_name?: string | null;
   created_at?: string;
   updated_at?: string;
   waiting_count: number;
+  revisions: TableRevision[];
 }
 interface TmpPath {
   category?: string;
   title?: string;
-  author?: string;
-  handle_name?: string;
+  author?: WriterUserInfo;
+  handle_name?: string | null;
   created_at?: string;
   updated_at?: string;
   waiting_count: number;
+  revisions: TableRevision[];
+}
+interface TableRevision extends BlogRevision {
+  _rowVariant?: BvTableVariant;
 }
 
 @Component({ components: { FetchStatusIcon, Breadcrumb } })
 export default class PathList extends Vue {
   readonly page_title = "記事一覧";
   paths: Path[] = [];
-  readonly fields = [
+  readonly pathFields = [
     { key: "waiting_count", label: "" },
     { key: "id", label: "Article ID" },
     { key: "category", formatter: this.get_category_name },
     { key: "title" },
-    { key: "author" },
+    { key: "author", formatter: this.get_author_name },
     { key: "handle_name" },
     { key: "created_at", formatter: this.get_formatted_time, sortable: true },
     { key: "updated_at", formatter: this.get_formatted_time, sortable: true },
     { key: "actions", label: "" },
   ];
-  filter = "";
+  readonly revisionFields = [
+    { key: "id", label: "No.", sortable: true },
+    { key: "status", label: "" },
+    { key: "title" },
+    { key: "author", formatter: this.get_author_name },
+    { key: "handle_name" },
+    {
+      key: "timestamp",
+      label: "Created At",
+      formatter: this.get_formatted_time,
+      sortable: true,
+    },
+    { key: "actions", label: "" },
+  ];
+  pathFilter = "";
   sortBy = "created_at";
 
   client = aspida();
@@ -163,20 +222,23 @@ export default class PathList extends Vue {
           .blog.articles.$get()
           .then((data: BlogArticle[]) => {
             for (const article of data) {
-              this.$set(tmp_paths, article.id, {
+              tmp_paths[article.id] = {
                 category: article.category,
                 title: article.title,
-                author: article.author.name,
+                author: article.author,
                 handle_name: article.handle_name,
                 created_at: article.created_at,
                 updated_at: article.updated_at,
                 waiting_count: 0,
-              });
+                revisions: [],
+              };
             }
           });
         return token;
       })
       .then((token) => {
+        // eslint-disable-next-line no-console
+        console.log(tmp_paths);
         // get revisions
         return api(this.client)
           .blog.revisions.$get({
@@ -184,21 +246,34 @@ export default class PathList extends Vue {
               "X-ADMIN-TOKEN": token.content,
             },
           })
-          .then((data: BlogRevision[]) => {
+          .then((data: TableRevision[]) => {
             for (const revision of data) {
               if (!(revision.article_id in tmp_paths)) {
                 // does not exist
-                this.$set(tmp_paths, revision.article_id, {
+                tmp_paths[revision.article_id] = {
                   title: revision.title,
                   created_at: revision.timestamp,
-                  author: revision.author.name,
+                  author: revision.author,
                   handle_name: revision.handle_name,
                   waiting_count: 0,
-                });
+                  revisions: [],
+                };
               }
               if (revision.status === "waiting") {
                 tmp_paths[revision.article_id].waiting_count++;
               }
+              switch (revision.status) {
+                case "accepted":
+                  revision["_rowVariant"] = "success";
+                  break;
+                case "rejected":
+                  revision["_rowVariant"] = "secondary";
+                  break;
+                case "waiting":
+                  revision["_rowVariant"] = "warning";
+                  break;
+              }
+              tmp_paths[revision.article_id].revisions.push(revision);
             }
           })
           .catch(() => {
@@ -218,6 +293,7 @@ export default class PathList extends Vue {
             created_at: path.created_at,
             updated_at: path.updated_at,
             waiting_count: path.waiting_count,
+            revisions: path.revisions,
           });
         }
         this.paths.splice(0, this.paths.length, ...array_paths);
@@ -244,8 +320,24 @@ export default class PathList extends Vue {
     return row.item.category;
   }
 
+  get_author_name(author: WriterUserInfo) {
+    return author.name;
+  }
+
   get_waiting_count(row: { item: Path }) {
     return row.item.waiting_count;
+  }
+
+  get_revisions(row: { item: Path }) {
+    return row.item.revisions;
+  }
+
+  toggle_details(row: { toggleDetails(): void }) {
+    return row.toggleDetails();
+  }
+
+  get_is_details_showing(row: { detailsShowing: boolean }) {
+    return row.detailsShowing;
   }
 
   get_category_name(category_id: string | undefined) {
@@ -254,6 +346,43 @@ export default class PathList extends Vue {
 
   get_formatted_time(timestamp: string | undefined) {
     return timestamp ? getStringTime(timestamp) : "-";
+  }
+
+  get_status_color(r_row: { item: BlogRevision }) {
+    switch (r_row.item.status) {
+      case "accepted":
+        return "success";
+      case "rejected":
+        return "secondary";
+    }
+    return "";
+  }
+
+  get_status_icon(r_row: { item: BlogRevision }) {
+    switch (r_row.item.status) {
+      case "accepted":
+        return "check-circle";
+      case "rejected":
+        return "times-circle";
+      case "waiting":
+        return "hourglass-half";
+    }
+  }
+
+  is_revision_accepted(r_row: { item: BlogRevision }) {
+    return r_row.item.status == "accepted";
+  }
+
+  is_revision_waiting(r_row: { item: BlogRevision }) {
+    return r_row.item.status === "waiting";
+  }
+
+  format_revision_icon_id(r_row: { item: BlogRevision }) {
+    return r_row.item.id + "-status-icon";
+  }
+
+  get_revision_status(r_row: { item: BlogRevision }) {
+    return r_row.item.status;
   }
 }
 </script>
