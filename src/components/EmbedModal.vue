416e01b80eb6e75cdde012bb59fa2c1e179dfc94
subaru <s@su8ru.dev>
2020-10-05 22:59:11 +0900

    feat: update selection modal

--- src/components/EmbedModal.vue
+++ src/components/EmbedModal.vue
@@ -13,17 +13,26 @@
         label-for="input-1"
         description="共有用URLを入力してください。"
       >
-        <b-form-input
-          id="input-url"
-          v-model="content"
-          type="text"
-          required
-        ></b-form-input>
+        <b-form-input id="input-url" v-model="content" type="text" required />
       </b-form-group>
     </b-form>
     <ul class="type-icons" :class="{ has_selection: type }">
-      <li v-for="(icon, i) in icons" :key="i" :class="get_icon_classes(i)">
-        <font-awesome-icon :icon="get_icon_icon(i)" class="fa-fw" />
+      <li
+        v-for="(example, i) in embedExamples"
+        :key="i"
+        :class="get_icon_classes(example)"
+      >
+        <font-awesome-icon
+          :icon="get_example_icon(example)"
+          class="fa-fw mr-1"
+        />
+        <span>{{ get_example_name(example) }}</span>
+        <br />
+        <ul class="example-url-wrapper">
+          <li v-for="url in get_example_example(example)" :key="url">
+            <code class="example-url">{{ url }}</code>
+          </li>
+        </ul>
       </li>
     </ul>
     <template v-slot:modal-footer="{ ok }">
@@ -32,43 +41,72 @@
   </b-modal>
 </template>
 
-<style lang="scss">
-.type-icons {
-  height: 2rem;
-  display: flex;
-  align-items: center;
-  padding: 0;
-  list-style: none;
+<style lang="scss" scoped>
+$twitter: #55acee;
+$instagram: #262626;
+$instagram-gradient: linear-gradient(-135deg, #1400c8, #b900b4, #f50000);
+$youtube: #ff0000;
+$link: #777777;
 
-  font-size: 1.2rem;
+.type-icons {
+  font-size: 1rem;
+  list-style: none;
+  padding: 0;
 
   li {
     transition: transform 0.3s ease-out, font-size 0.3s ease-out;
-    display: inline-block;
+    padding: 5px 10px;
+    border-radius: 8px;
+
+    .example-url-wrapper {
+      margin: 0;
+      padding: 0;
+
+      li {
+        display: block;
+        list-style: none;
+        padding: 0;
+      }
+    }
   }
 
-  &.has_selection li {
-    font-size: 0.75em;
-    &.selected {
-      font-size: 1.5em;
-    }
-    &:not(.selected) {
-      opacity: 0.5;
-    }
-  }
-
+  // 非選択時
   .tweet,
   .twitter_timeline {
-    color: #55acee;
+    color: $twitter;
   }
   .instagram {
-    color: #262626;
+    color: $instagram;
   }
   .youtube {
-    color: #ff0000;
+    color: $youtube;
   }
   .link {
-    color: #888;
+    color: $link;
+  }
+
+  &.has_selection > li {
+    &.selected {
+      color: #fff;
+      padding: 10px 10px;
+
+      &.tweet,
+      &.twitter_timeline {
+        background: $twitter;
+      }
+      &.instagram {
+        background: $instagram-gradient;
+      }
+      &.youtube {
+        background: $youtube;
+      }
+      &.link {
+        background: $link;
+      }
+    }
+    &:not(.selected) {
+      opacity: 0.4;
+    }
   }
 }
 </style>
@@ -81,7 +119,7 @@ import {
   faInstagram,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faLink, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 type EmbedType =
   | "tweet"
@@ -92,6 +130,12 @@ type EmbedType =
 type EmbedInfo = {
   type: EmbedType;
   content: string;
+};
+type EmbedExample = {
+  type: EmbedType;
+  icon: IconDefinition;
+  name: string;
+  example: string[];
 };
 
 const leading_https = /^(https?:)?\/\/(www\.)?/;
@@ -150,13 +194,41 @@ function detect_embed_type(content: string): EmbedInfo {
 @Component
 export default class EmbedModal extends Vue {
   content = "";
-  readonly icons = [
-    { type: "tweet", icon: faTwitter },
-    { type: "twitter_timeline", icon: faTwitterSquare },
-    { type: "instagram", icon: faInstagram },
-    { type: "youtube", icon: faYoutube },
-    { type: "link", icon: faLink },
-  ];
+  readonly embedExamples = [
+    {
+      type: "tweet",
+      icon: faTwitter,
+      name: "Twitter tweet",
+      example: ["https://twitter.com/73_Afes/status/1311653798381674499"],
+    },
+    {
+      type: "twitter_timeline",
+      icon: faTwitterSquare,
+      name: "Twitter user timeline",
+      example: ["https://twitter.com/73_Afes"],
+    },
+    {
+      type: "instagram",
+      icon: faInstagram,
+      name: "Instagram post",
+      example: ["https://www.instagram.com/p/B9LVH_Oh1rW/"],
+    },
+    {
+      type: "youtube",
+      icon: faYoutube,
+      name: "YouTube video & live",
+      example: [
+        "https://www.youtube.com/watch?v=hy6B_EPE50s",
+        "https://youtu.be/hy6B_EPE50s",
+      ],
+    },
+    {
+      type: "link",
+      icon: faLink,
+      name: "その他 URL（リンクとして挿入）",
+      example: ["https://afes.info", "https://github.com/afes-website/front/"],
+    },
+  ] as EmbedExample[];
 
   @Prop({ default: false })
   value = false;
@@ -196,22 +268,21 @@ export default class EmbedModal extends Vue {
     this.content = "";
   }
 
-  get_icon_type(idx: number) {
-    return this.icons[idx].type;
+  get_example_icon(example: EmbedExample) {
+    return example.icon;
+  }
+  get_example_name(example: EmbedExample) {
+    return example.name;
+  }
+  get_example_example(example: EmbedExample) {
+    return example.example;
   }
 
-  get_icon_icon(idx: number) {
-    return this.icons[idx].icon;
+  is_selected(example: EmbedType) {
+    return this.type === example;
   }
-
-  is_selected(type: EmbedType) {
-    return this.type === type;
-  }
-  get_icon_classes(idx: number) {
-    return [
-      { selected: this.icons[idx].type === this.type },
-      this.icons[idx].type,
-    ];
+  get_icon_classes(example: EmbedExample) {
+    return [{ selected: example.type === this.type }, example.type];
   }
 }
 </script>
